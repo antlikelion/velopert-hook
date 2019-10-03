5092f4154a0c664325bde1efdd03685cb3ecc2d5
antlikelion <ignorant_woo@naver.com>
2019-10-04 03:11:22 +0900

    컴포넌트가 언마운트 되기 전, 혹은 업데이트 되기 전을 다루는 useEffect

--- src/Info.js
+++ src/Info.js
@@ -5,9 +5,17 @@ const Info = () =>{
     const [nickName, setNickName] = useState('');
 
     useEffect(()=>{
+        console.log('effect');
         console.log(name);
+        return () => {
+            console.log('cleanup');
+            console.log(name);
+        }
     },[name])
-    // useEffect의 dependency로 name이 담긴 배열을 전달해줬기에, 마운트 된 이후로는 name의 갱신에만 반응한다.
+    // useEffect는 기본적으로 렌더링 되고난 직후마다 실행된다.
+    // 컴포넌트가 언마운트되기 전이나, 업데이트 되기 직전에 특정 작업을 수행하고 싶다면
+    // useEffect에서 cleanup함수를 반환해야 한다.
+    // + 컴포넌트가 언마운트될 때에만 cleanup함수를 실행하려면 dependency로 빈 배열을 전하면 된다
 
     const onChangeName = e => {
         setName(e.target.value);
